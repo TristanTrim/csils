efb2d380922f4ad5bb44c399b627a37e73186063
Tristan Trim <tTrim@linamar.com>
2020-05-14 13:48:28 -0400

    basic 'modes'

--- csils.py
+++ csils.py
@@ -4,6 +4,7 @@ import threading
 from time import sleep
 import subprocess
 import msvcrt # needs unix portablility
+import json
 
 # terminal line control hack
 _=subprocess.call("",shell=True)
@@ -17,21 +18,34 @@ print("\n"*lines)
 display_rate = .2
 convo_rate = .01
 running = True
+default_conf_file = "conf"
 
 #menu vars
 info={
-        "help":lambda:"q-quit c-config ?-help",
-        "config":lambda:"n-name c-comm b-baud"
+        "help":lambda:"q-quit c-config",
+        "config":lambda:"n-name c-comm b-baud m-main",
+        "conf":lambda:str(conf),
         }
 curr_info="help"
+curr_mode="main"
+
+#load default conf
+conf=json.load(open("conf",'r'))
 
 def updateDisplay():
-    global curr_info
+    global curr_info, curr_mode
     #clear
     print("\033[F\033[K"*lines,end="")
     print(info[curr_info]())
-    for ii in range(lines-1):
-        print("%8d"%count,"foo")
+    if(curr_mode=="main"):
+        for ii in range(lines-1):
+            print("%8d"%count,"foo")
+    elif(curr_mode=="config"):
+        print(conf["dev1"])
+        print(conf["dev2"])
+        for ii in range(lines-1-2):
+            print("%8d"%count,"foo")
+
 def displayLoop():
     global running
     while running:
@@ -57,10 +71,14 @@ while(running):
     inp=msvcrt.getch()
     if(inp==b"q"):
         running=False
-    elif(inp==b"c"):
-        curr_info="config"
-    elif(inp==b"?"):
-        curr_info="help"
+    elif curr_mode=="main":
+        if(inp==b"c"):
+            curr_info="config"
+            curr_mode="config"
+    elif curr_mode=="config":
+        if(inp==b"m"):
+            curr_info="help"
+            curr_mode="main"
 
 
 
