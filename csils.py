4ff53ed7eb8fd4ec6c6437a716a66c13dbc0c024
Tristan Trim <tTrim@linamar.com>
2020-05-14 12:36:59 -0400

    Basic command loop with seperate thread for convo logic

--- csils.py
+++ csils.py
@@ -1,15 +1,34 @@
 
 
 import threading
-import msvcrt
 from time import sleep
+import subprocess
+import msvcrt # needs unix portablility
 
+# terminal line control hack
+_=subprocess.call("",shell=True)
+
+# data vars
+count = 0
+
+# config vars
+lines = 13
+print("\n"*lines)
 running = True
+
+def clear():
+    print("\033[F\033[K"*lines,end="")
+def updateDisplay():
+    clear()
+    for ii in range(lines):
+        print(count,"foo")
 
 def convoLoop():
     global running
+    global count
     while(running):
-        print("Hi",end="",flush=True)
+        count+=1
+        updateDisplay()
         sleep(2)
 convoThread = threading.Thread(target=convoLoop)
 convoThread.start()
