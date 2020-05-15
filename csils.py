4cd1a4395b8aefe2024ccfa9cf25d6041c06b088
TristanTrim <tristantrim@gmail.com>
2020-05-15 16:53:11 -0400

    compatible with linux now

--- csils.py
+++ csils.py
@@ -5,7 +5,25 @@ import traceback
 import threading
 from time import sleep, time
 import subprocess
-import msvcrt # needs unix portablility
+try:
+    import msvcrt # windows
+    OS = "windows"
+    def getch():
+        return(msvcrt.getch().decode("utf-8"))
+except ModuleNotFoundError:
+    # linux
+    OS = "linux"
+    import sys, tty, termios
+    def getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
 import json
 
 # get the size of the terminal
@@ -62,8 +80,9 @@ def updateDisplay():
     #clear
     print("\033[F\033[K"*lines,end="")
     print(info[curr_info]())
+    print("\r",end="")
     if(curr_mode=="main" or curr_mode=="tags"):
-        for ii in range(log_offset,log_offset+lines-1):
+        for ii in range(log_offset,min(log_offset+lines-1,len(convo_log))):
             line = convo_log[ii]
             if ii == main_curr:
                 print("\033[1;37m",end="")#white
@@ -77,14 +96,14 @@ def updateDisplay():
                     +"... "\
                     +"%8s"%line[3]
             print(outputLine[:columns])
-        print("\033[0;37m",end="")#grey
+        print("\033[0;37m\r",end="")#grey
         for ii in range(lines-1-len(convo_log)):
-            print("...")
+            print("...\r")
     elif(curr_mode=="config"):
-        print(" ",conf["dev1"])
-        print(" ",conf["dev2"])
+        print(" ",conf["dev1"],"\r")
+        print(" ",conf["dev2"],"\r")
         for ii in range(lines-1-2):
-            print("%8d"%count,"foo")
+            print("%8d"%count,"foo","\r")
 
 def displayLoop():
     global running
@@ -143,16 +162,16 @@ convoThread.start()
 try:
     gg=False
     while(running):
-        inp=msvcrt.getch()
-        if(inp==b"q"):
+        inp=getch()
+        if(inp=="q"):
             running=False
-        elif(inp==b"m"):
+        elif(inp=="m"):
             curr_info="main"
             curr_mode="main"
         elif curr_mode=="main":
             ### motion controls ###
             ## gg latch ##
-            if(inp==b"g"):
+            if(inp=="g"):
                 if(gg):
                     main_curr=0
                     log_offset=0
@@ -161,61 +180,64 @@ try:
             else:
                 gg=False
             ## other stuff ##
-            if(inp==b"j"):
+            if(inp=="j"):
                 main_curr+=1
                 if(main_curr==len(convo_log)):
                     main_curr=len(convo_log)-1
                 elif(main_curr>log_offset+lines-2):
                     log_offset+=1
-            elif(inp==b"k"):
+            elif(inp=="k"):
                 main_curr-=1
                 if(main_curr==-1):
                     main_curr=0
                 elif(main_curr<log_offset):
                     log_offset-=1
-            elif(inp==b"G"):
+            elif(inp=="G"):
                 main_curr=len(convo_log)-1
                 log_offset=len(convo_log)-lines+1
             ### other main stuff ###
-            elif(inp==b"c"):
+            elif(inp=="c"):
                 curr_info="config"
                 curr_mode="config"
-            elif(inp==b"t"):
+            elif(inp=="t"):
                 curr_mode="tags"
                 curr_info="tags"
         elif curr_mode=="config":
-            if(inp==b"c"):
+            if(inp=="c"):
                 prev_info=curr_info
                 curr_info="connect"
-                inp=msvcrt.getch()
-                if(inp==b"1"):
+                inp=getch()
+                if(inp=="1"):
                     dev1=serial.Serial( *conf["dev1"][1], timeout=0)
-                elif(inp==b"2"):
+                elif(inp=="2"):
                     dev1=serial.Serial( *conf["dev2"][1], timeout=0)
-                elif(inp==b"a"):
+                elif(inp=="a"):
                     dev1=serial.Serial( *conf["dev1"][1], timeout=0)
                     dev1=serial.Serial( *conf["dev2"][1], timeout=0)
                 curr_info=prev_info
-            elif(inp==b"n"):
+            elif(inp=="n"):
                 pass
-            elif(inp==b"p"):
+            elif(inp=="p"):
                 pass
-            elif(inp==b"b"):
+            elif(inp=="b"):
                 pass
         elif curr_mode=="tags":
-            if(inp==b"n"):
+            if(inp=="n"):
                 curr_info="newtag"
                 entry_buf=""
                 while True:
-                    inp=msvcrt.getch()
-                    if(inp==b"\r"):#enter
+                    inp=getch()
+                    if(inp=="\r"):#enter
                         tags+=[entry_buf]
                         curr_mode=curr_info="main"
                         break
-                    elif(inp==b"\x08"):#backspace
+                    # windows and linux seem to have diff backspace?
+                    # or is it my keyboard?
+                    # anyway:  windows        linux
+                    elif(inp=="\x08" or inp=="\x7f"):#backspace
                         entry_buf=entry_buf[:-1]
                     else:
-                        entry_buf+=inp.decode("utf-8")
+                        entry_buf+=inp
 except Exception:
     print(traceback.format_exc())
     running=False
